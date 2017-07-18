769dc646bd7a5880fcdc1a36b8c9e8cb03f2d996
kalyandechiraju <kalyandechiraju@outlook.com>
2017-07-18 07:14:52 +0530

    Initial Commit

---
+++ app/src/main/java/com/kalyan/videonotes/Keys.java
@@ -0,0 +1,10 @@
+package com.kalyan.videonotes;
+
+/**
+ * Copyright (c) 2017 Codelight Studios
+ * Created by kalyandechiraju on 18/07/17.
+ */
+
+public class Keys {
+    public static final String YT_DEV_KEY = null;
+}
