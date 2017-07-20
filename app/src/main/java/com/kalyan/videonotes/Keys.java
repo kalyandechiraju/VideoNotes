7842eac20f340fee1acb282d3dda743fc0e91cdb
Kalyan Dechiraju <kalyandechiraju@outlook.com>
2017-07-21 01:35:23 +0530

    Update Keys.java

--- app/src/main/java/com/kalyan/videonotes/Keys.java
+++ app/src/main/java/com/kalyan/videonotes/Keys.java
@@ -1,10 +1,10 @@
 package com.kalyan.videonotes;
 
 /**
- * Copyright (c) 2017 Codelight Studios
  * Created by kalyandechiraju on 18/07/17.
  */
 
 public class Keys {
-    public static final String YT_DEV_KEY = null;
+    public static final String YT_DEV_KEY = "INSERT_KEY_HERE";
+    public static final String MSFT_SPEECH_KEY = "INSERT_KEY_HERE";
 }
